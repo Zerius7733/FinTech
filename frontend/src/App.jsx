f9e5ce84f7c87ea8b2f719f4038362de7ffd7cac
simplyziannn <160417285+simplyziannn@users.noreply.github.com>
2026-03-09 03:09:42 +0800

    fix get started button from market

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,36 @@ import Commodities from './pages/Commodities.jsx'
 import Crypto  from './pages/Crypto.jsx'
 import Login   from './pages/Login.jsx'
 import Theme   from './pages/Theme.jsx'
+import LoginModal from './components/LoginModal.jsx'
+import SurveyModal from './components/SurveyModal.jsx'
+import { useLoginModal } from './context/LoginModalContext.jsx'
 
 export default function App() {
+  const { loginModalOpen, setLoginModalOpen, surveyModalOpen, setSurveyModalOpen } = useLoginModal()
+
   return (
-    <Routes>
-      <Route path="/"         element={<Globe />} />
-      <Route path="/login"    element={<Login />} />
-      <Route path="/survey"   element={<Survey />} />
-      <Route path="/profile"  element={<Profile />} />
-      <Route path="/stocks"   element={<Stocks />} />
-      <Route path="/commodities" element={<Commodities />} />
-      <Route path="/crypto"   element={<Crypto />} />
-      <Route path="/theme"    element={<Theme />} />
-      <Route path="*"         element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/"         element={<Globe />} />
+        <Route path="/login"    element={<Login />} />
+        <Route path="/survey"   element={<Survey />} />
+        <Route path="/profile"  element={<Profile />} />
+        <Route path="/stocks"   element={<Stocks />} />
+        <Route path="/commodities" element={<Commodities />} />
+        <Route path="/crypto"   element={<Crypto />} />
+        <Route path="/theme"    element={<Theme />} />
+        <Route path="*"         element={<Navigate to="/" replace />} />
+      </Routes>
+
+      <LoginModal
+        open={loginModalOpen}
+        onClose={() => setLoginModalOpen(false)}
+        onOpenSurvey={() => setSurveyModalOpen(true)}
+      />
+      <SurveyModal
+        open={surveyModalOpen}
+        onClose={() => setSurveyModalOpen(false)}
+      />
+    </>
   )
 }
